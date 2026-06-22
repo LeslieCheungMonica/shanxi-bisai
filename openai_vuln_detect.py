71e5f5782c374349127ca22e2b6d76532b5278c1
李彦辉 <li.yh9@asiainfo-sec.com>
2026-06-22 13:43:44 +0800

    first commit

--- openai_vuln_detect.py
+++ openai_vuln_detect.py
@@ -1,7 +1,7 @@
 import os
 from openai import OpenAI
 
-client = OpenAI(base_url="http://0.0.0.0.0.0:899/v1", api_key="token-sdc123")
+client = OpenAI(base_url="http://0.0.0.0:8999/v1", api_key="token-sdc123")
 
 files_dir = "files"
 output_dir = "output"
